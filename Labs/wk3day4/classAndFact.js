01d4d77ee37db21a8f8cec072107b86ea911cd20
LAaronMcKnight <110464137+LAaronMcKnight@users.noreply.github.com>
2022-08-26 09:36:42 -0400

    added person class

--- Labs/wk3day4/classAndFact.js
+++ Labs/wk3day4/classAndFact.js
@@ -15,4 +15,49 @@ class Hamster {
     }
 }
 
-Hamster.name = 'Ham'
+// ----------------------------------------add person
+
+
+class Person {
+    constructor(John){
+        this.name = 'John'
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamsters = []
+        this.bankAccount = 0
+    }
+
+    getName(){
+        return this.name
+    }
+
+    getAge(){
+        return this.age
+    }
+
+    getWeight(){
+        return this.weight
+    }
+
+    greet(){
+        console.log(`Welcome, ${this.name}!`)
+    }
+
+    eat(){
+        this.weight++, this.mood++
+    }
+
+    exercise(){
+        this.weight--
+    }
+
+    ageUp(){
+        this.age++, this.height++, this.weight++, this.mood--, this.bankAccount + 10
+    }
+    buyHamster(){
+        this.hamsters.push(Hamster)
+    }
+        
+}

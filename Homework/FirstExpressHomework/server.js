4f82be80dec8da87a27e2aadcfcca339ba11449b
LAaronMcKnight <110464137+LAaronMcKnight@users.noreply.github.com>
2022-09-15 17:04:39 -0400

    Tip Calculator express application created

--- Homework/FirstExpressHomework/server.js
+++ Homework/FirstExpressHomework/server.js
@@ -8,7 +8,9 @@ app.get('/greeting/:name', (req, res) =>{
 })
 
 
-
+app.get ('/tip/:total/:tipPercentage', (req, res)=>{
+    res.send(`The tip for a bill totalling $${req.params.total} with a tip of ${req.params.tipPercentage}% is $${req.params.total / 100 * req.params.tipPercentage}`)
+})
 
 
 
